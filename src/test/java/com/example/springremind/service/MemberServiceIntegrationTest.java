c8c3b0a39c266b5a395aaf5bf6799543a040006c
SeoHyeokGyu <shk64099@gmail.com>
2021-11-25 16:43:59 +0900

    #Add 1. 통합테스트 추가 2. 테스트에 @Transactional 걸면 테스트가 끝나면 롤백해준다.

---
+++ src/test/java/com/example/springremind/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,60 @@
+package com.example.springremind.service;
+
+import com.example.springremind.domain.Member;
+import com.example.springremind.repository.MemberRepository;
+import com.example.springremind.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @Test
+    void join() {
+
+        Member member = new Member();
+        member.setName("hello");
+
+        Long saveId = memberService.join(member);
+
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void validationDuple() {
+        //g
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //w
+        memberService.join(member1);
+
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        //t
+
+    }
+
+}
